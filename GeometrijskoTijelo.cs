063b92d771f6a88406f2e90dd97a55156f3f85e7
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-12 16:02:32 +0100

    Add project files.

---
+++ GeometrijskoTijelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavljanjeZadatak7
+{
+    class GeometrijskoTijelo
+    {
+        public double Volumen;
+        public double Oplosje;
+        public static int BrojacTijela;
+        public GeometrijskoTijelo()
+            {
+            BrojacTijela++;
+            }
+        public override string ToString()
+        {
+            string suma = ""+BrojacTijela;
+            return suma;
+        }
+    }
+}
